32870d25a3a9f53916afcd73e9573ac1d41e0a08
mullerow <maximilian.kohler@posteo.de>
2024-02-28 16:05:24 +0100

    feat: zip-strings and extract-class-names are solved

--- 01-zip-strings/zip-strings.js
+++ 01-zip-strings/zip-strings.js
@@ -1,3 +1,68 @@
+/*
+
 function zipStrings(strA, strB) {
-  // Your Code here
+  console.log(strA + "|||" + strB);
+  let part = "";
+  let result = "";
+  let difference = Math.abs(strA.length - strB.length);
+  console.log("diff", difference);
+
+  if (strA.length >= strB.length) {
+    for (let i = 0; i < strA.length; i++) {
+      part = strA[i] + strB[i];
+      console.log("part", part);
+      result += part;
+      if (i === strB.length - 1) {
+        break;
+      }
+    }
+    for (let x = strA.length - difference; x < strA.length; x++) {
+      console.log("x", x);
+      result += strA[x];
+    }
+  } else {
+    for (let i = 0; i < strB.length; i++) {
+      part = strA[i] + strB[i];
+      console.log("part", part);
+      result += part;
+      console.log("test", strA.length, i);
+      if (i === strA.length - 1) {
+        break;
+      }
+    }
+    console.log("strB.length - difference", strB.length - difference - 1);
+    for (x = strB.length - difference; x < strB.length; x++) {
+      console.log("x", x);
+      result += strB[x];
+    }
+  }
+
+  if (strA.length === 0) return strB;
+  if (strB.length === 0) return strA;
+
+  console.log("result:", result);
+  return result;
+}
+
+*/
+
+function zipStrings(strA, strB) {
+  let result = "";
+  arrA = strA.split("");
+  arrB = strB.split("");
+  let delta = Math.abs(strA.length - strB.length);
+  if (strA.length >= strB.length) {
+    for (let x = strA.length - delta; x < strA.length; x++) {
+      arrB[x] = "";
+    }
+  }
+  if (strB.length >= strA.length) {
+    for (let x = strB.length - delta; x < strB.length; x++) {
+      arrA[x] = "";
+    }
+  }
+  for (let i = 0; i < arrA.length; i++) {
+    result += arrA[i] + arrB[i];
+  }
+  return result;
 }
--- 02-extract-class-name/extract-class-name.js
+++ 02-extract-class-name/extract-class-name.js
@@ -1,3 +1,56 @@
 function extractClassName(sessionTitle) {
-  // Add you solution here
+  let array = sessionTitle.split(" ");
+  let month = "";
+  let year = 0;
+
+  if (array[2].length === 4 && array[2][0] === "2") {
+    year = array[2];
+  }
+  if (array[3]) {
+    switch (array[3]) {
+      case "Januar":
+        month = "01";
+        break;
+      case "Februar":
+        month = "02";
+        break;
+      case "März":
+        month = "03";
+        break;
+      case "Maerz":
+        month = "03";
+        break;
+      case "April":
+        month = "04";
+        break;
+      case "Mai":
+        month = "05";
+        break;
+      case "Juni":
+        month = "06";
+        break;
+      case "Juli":
+        month = "07";
+        break;
+      case "August":
+        month = "08";
+        break;
+      case "September":
+        month = "09";
+        break;
+      case "Oktober":
+        month = "10";
+        break;
+      case "November":
+        month = "11";
+        break;
+      default:
+        month = "12";
+        break;
+    }
+  }
+  if (year != 0 && month != "") {
+    console.log("class", year + "-" + month);
+    return year + "-" + month;
+  } else return null;
 }
